4a9cb8fe8ad2fed5a2aabeacca0a2bc85655ecc9
Josetoespinosa <jtespinosa1@miuandes.cl>
2024-09-15 12:05:34 -0300

    Ubicacion del ususario

--- www-frontend/src/components/Home.jsx
+++ www-frontend/src/components/Home.jsx
@@ -6,6 +6,9 @@ import './SharedStyles.css';
 function Home() {
   const [currentUser, setCurrentUser] = useState(null);
   const [bars, setBars] = useState([]); 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [currentUserLocation, setCurrentUserLocation] = useState({ lat: 0, lng: 0 });
+
 
   useEffect(() => {
     fetch('http://localhost:3001/api/v1/current_user', {
@@ -28,27 +31,63 @@ function Home() {
     .then(data => setBars(data.bars))
     .catch(error => console.error('Error fetching bars:', error));
   }, []);
-  console.log(JSON.stringify(bars));
+
+  const filteredBars = bars.filter(bar => 
+    bar.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  useEffect(() => {
+    // Solicitar la ubicación del usuario
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setCurrentUserLocation({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          });
+        },
+        () => {
+          console.error("Permission denied or error obtaining location");
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
 
   
   return (
     <div>
-      <h1 className='title'>Welcome to Beer App, {currentUser?.handle}</h1>
+      <div>
+      <h1 className='title'>Welcome to Beer App {currentUser?.handle}</h1>
+      <input
+          type="text"
+          placeholder="Search bars..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
       <APIProvider apiKey={'AIzaSyCSurJdzxphPzLeVH_vbJg3u_WlDXKlxrI'}>
       <Map
-        zoom={1}
-        center={{ lat: -17.61345692092152, lng:28.971957221315847}}
+        zoom={13}
+        center={currentUserLocation}
         mapId={'7ef791e0ee46cce9'}
-        style={{ width: '50vw', height: '50vh', border: '50' }}
+        style={{ width: '25vw', height: '60vh', borderWidth: '2px', // Ancho del borde
+          borderStyle: 'solid', // Estilo del borde
+          borderColor: '#000000', 
+          borderRadius: '25px',
+        overflow: 'hidden' }} // Propiedades de estilo del mapa
         // Propiedades para habilitar la interacción del usuario
         zoomControl={true}
-        mapTypeControl={true}
+        mapTypeControl={true} 
         scaleControl={true}
         streetViewControl={true}
         rotateControl={true}
         gestureHandling="auto" // Permite la interacción con gestos del mouse y táctiles
       >
-        {bars.map(bar => (
+        <AdvancedMarker position={currentUserLocation}>
+            <Pin background={'#0000FF'} glyphColor={'#000'} borderColor={'#000'} />
+        </AdvancedMarker>
+        {filteredBars.map(bar => (
         <AdvancedMarker position={{ lat: bar.latitude, lng:bar.longitude}}>
             <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
         </AdvancedMarker>
